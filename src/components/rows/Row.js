b7328fe0979b93edcb860286df3eecb0a1f3ea41
MikeJonesSE <mikejones.sedev@gmail.com>
2021-08-15 18:16:42 -0700

    redeployed

--- src/components/rows/Row.js
+++ src/components/rows/Row.js
@@ -30,7 +30,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
     if (trailerUrl) {
       setTrailerUrl("");
     } else {
-      movieTrailer(movie?.title || "")
+      movieTrailer(movie?.title || movie?.name || movie?.original_name || "")
         .then((url) => {
           const urlParams = new URLSearchParams(new URL(url).search);
           setTrailerUrl(urlParams.get("v"));
